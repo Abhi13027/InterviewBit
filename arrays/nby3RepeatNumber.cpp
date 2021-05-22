712f5bfccc03c072cb826cafc8ee66b4a3246d9a
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-05-22 11:00:16 +0530

    Solved the n/3 repeat number problem

---
+++ arrays/nby3RepeatNumber.cpp
@@ -0,0 +1,44 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+  int n = A.size();
+  
+  int first = INT_MAX, second = INT_MAX;
+  int count1=0, count2=0;
+  for(int i=0;i<n;i++){
+      if(A[i] == first){
+          count1++;
+      }
+      else if(A[i] == second){
+          count2++;
+      }
+      else if(count1==0){
+          count1++;
+          first=A[i];
+      }
+      else if(count2==0){
+          count2++;
+          second = A[i];
+      }
+      else{
+          count1--;
+          count2--;
+      }
+  }
+  count1 =0, count2=0;
+  for(int i=0;i<n;i++){
+      if(A[i]== first){
+          count1++;
+      }
+      else if(A[i] == second) {
+            count2++;
+      }
+  }
+  
+  if(count1>n/3){
+      return first;
+  }
+  if(count2>n/3){
+      return second;
+  }
+  return -1;
+}
+
