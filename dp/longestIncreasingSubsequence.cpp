34cd1f86efcba812433aec95d03b146829af4f14
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-06-02 11:18:58 +0530

    Solved the longest Increasing Subsequence Problem

---
+++ dp/longestIncreasingSubsequence.cpp
@@ -0,0 +1,24 @@
+int Solution::lis(const vector<int> &A) {
+    int n = A.size();
+    int lis[n];
+    for(int i=0;i<n;i++){
+        lis[i] = 1;
+    }
+    
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(A[i]> A[j] && lis[i] < lis[j] + 1){
+                lis[i] = lis[j] + 1;
+            }
+        }
+    }
+    
+    int max = lis[0];
+    for(int i=1;i<n;i++){
+        if(lis[i] > max){
+            max = lis[i];
+        }
+    }
+    return max;
+}
+
